817241974c7d48d26fc99a8a227c10511e8ba0e3
Tony <tonywang824@gmail.com>
2017-08-10 11:55:13 -0300

    Init commit

---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject accumulation "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
+                 [org.clojure/test.check "0.9.0"]
+                 [prismatic/schema "1.1.6"]
+                 [ring/ring-codec "1.0.1"]
+                 [clj-time "0.13.0"]
+                 [compojure "1.6.0"]
+                 [clj-http "3.6.1"]
+                 [robert/bruce "0.8.0"]
+                 [org.clojure/core.async "0.3.442"]
+                 [cheshire "5.7.1"]
+                 [com.cemerick/url "0.1.1"]
+                 [danlentz/clj-uuid "0.1.7"]
+                 [org.clojure/core.match "0.2.2"] 
+                 [commons-codec/commons-codec "1.10"]
+                 [com.taoensso/carmine "2.16.0"]
+                 [clojure-csv/clojure-csv "2.0.2"]
+                 [me.raynes/fs "1.4.6"]
+                 [com.velisco/clj-ftp "0.3.8"]
+                 [clj-ssh "0.5.14"]])
---
+++ test/accumulation/core_test.clj
@@ -0,0 +1,7 @@
+(ns accumulation.core-test
+  (:require [clojure.test :refer :all]
+            [accumulation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
